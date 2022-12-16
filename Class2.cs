646a310d7a6a342839fad52a92fc70258d71e9b0
Елинка <113973001+ChernoglazovaElena@users.noreply.github.com>
2022-12-16 21:59:10 +0300

    Add files via upload

---
+++ Class2.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace проводник
+{
+    public class Cursor
+    {
+        public int max, min, pos;
+        public Cursor(int max, int min, int pos)
+        {
+            this.max = max;
+            this.min = min;
+            this.pos = pos;
+        }
+    }
+}
